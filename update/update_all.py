92afc1af813dbbcdf973fe59bae3f88ad0b598b0
edbowker <edwardjobo@gmail.com>
2026-05-19 21:37:56 -0500

    Monthly report added to update flow (also content update)

--- update/update_all.py
+++ update/update_all.py
@@ -6,6 +6,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 from insights import generate_insights_cards
 from insights import generate_insights_colors
 from insights import generate_insights_sets
+from insights import generate_monthly_report
 from update import update_ids_lists
 from update import update_card_database
 
@@ -24,3 +25,5 @@ print('Recreating local data files...')
 generate_insights_cards.main()
 generate_insights_colors.main()
 generate_insights_sets.main()
+generate_monthly_report.main()
+
